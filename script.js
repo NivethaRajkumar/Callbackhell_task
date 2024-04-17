051ea6f4029e9faeac06a2691a580a271f7a5389
NivethaRajkumar <nivethas1207@gmail.com>
2024-04-17 12:40:55 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,36 @@
+setTimeout(()=>{
+    document.getElementById("callbackhell").innerHTML="<b>10</b>"
+    setTimeout(()=>{
+        document.getElementById("callbackhell").innerHTML="<b>9</b>"
+        setTimeout(()=>{
+            document.getElementById("callbackhell").innerHTML="<b>8</b>"
+            setTimeout(()=>{
+                document.getElementById("callbackhell").innerHTML="<b>7</b>"
+                setTimeout(()=>{
+                    document.getElementById("callbackhell").innerHTML="<b>6</b>"
+                    setTimeout(()=>{
+                        document.getElementById("callbackhell").innerHTML="<b>5</b>"
+                        setTimeout(()=>{
+                            document.getElementById("callbackhell").innerHTML="<b>4</b>"
+                            setTimeout(() => {
+                                document.getElementById("callbackhell").innerHTML="<b>3</b>"
+                                setTimeout(()=>{
+                                    document.getElementById("callbackhell").innerHTML="<b>2</b>"
+                                    setTimeout(()=>{
+                                        document.getElementById("callbackhell").innerHTML="<b>1</b>"
+                                        setTimeout(()=>{
+                                            document.getElementById("callbackhell").innerHTML="<b>Happy Independence Day</b>"
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+
+                    },1000);
+                },1000);
+            },1000);
+
+        },1000);
+    },1000);
+    
+},1000);
+},1000);
